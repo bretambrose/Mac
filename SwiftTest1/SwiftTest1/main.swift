c82d4b2b11d10866b87ee835255254a8716dd3a9
Bret Ambrose Mac <bretambrose@gmail.com>
2014-07-26 11:14:06 -0400

    Test Commit
    Testing

---
+++ SwiftTest1/SwiftTest1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftTest1
+//
+//  Created by Bret Ambrose on 7/26/14.
+//  Copyright (c) 2014 IP. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
